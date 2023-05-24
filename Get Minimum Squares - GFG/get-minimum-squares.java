358943e7e1f0a7af141283bcebcf4420ef1c8c83
Himanshu <95352548+himanshu-rai1121@users.noreply.github.com>
2023-05-24 13:16:45 +0530

    Added solution - LeetHub

---
+++ Get Minimum Squares - GFG/get-minimum-squares.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int ans = ob.MinSquares(n);
+            System.out.println(ans);           
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    // int solve(int n)
+    // {
+    //     if(n==0)
+    //         return 0;
+            
+    //     int ans = n;
+    //     for(int i=1;i*i<=n;i++)
+    //     {
+    //         ans = Math.min(ans, 1+solve(n-(i*i)) );
+    //     }
+    //     return ans;
+    // }
+    
+    int solvedp(int n, int[] dp)
+    {
+        if(n==0)
+            return 0;
+        
+        if(dp[n]!=-1)
+            return dp[n];
+        int ans = n;
+        for(int i=1;i*i<=n;i++)
+        {
+            ans = Math.min(ans, 1+solvedp(n-(i*i), dp) );
+        }
+        dp[n] = ans;
+        return ans;
+    }
+    public int MinSquares(int n)
+    {
+        // Code here
+        // return solve(n);
+        
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return solvedp(n, dp);
+    }
+}
