4f57a0deeb6c73ae43582774b5cd995010bbb686
Himanshu <95352548+himanshu-rai1121@users.noreply.github.com>
2023-05-25 00:44:52 +0530

    Time: 3 ms (55.92%), Space: 40.5 MB (26.53%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.java
@@ -12,26 +12,49 @@ class Solution {
     //     return ans;
     // }
     
-    int solvedp(int[] values, int i, int j, int[][] dp)
+    // int solvedp(int[] values, int i, int j, int[][] dp)
+    // {
+    //     if(i+1==j)
+    //         return 0;
+    //     int ans = Integer.MAX_VALUE;
+    //     if(dp[i][j]!=-1)
+    //         return dp[i][j];
+    //     for(int k=i+1;k<j;k++)
+    //     {
+    //         ans = Math.min(ans, values[i]*values[k]*values[j] + solvedp(values, i, k, dp) + solvedp(values, k, j, dp));
+    //     }
+    //     dp[i][j] = ans;
+    //     return dp[i][j];
+    // }
+    
+    int solvetab(int[] values)
     {
-        if(i+1==j)
-            return 0;
-        int ans = Integer.MAX_VALUE;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        for(int k=i+1;k<j;k++)
+        int n = values.length;
+        int[][] dp = new int[n][n];
+        
+        
+        for(int i=n-1;i>=0;i--)
         {
-            ans = Math.min(ans, values[i]*values[k]*values[j] + solvedp(values, i, k, dp) + solvedp(values, k, j, dp));
+            for(int j=i+2;j<n;j++)
+            {
+                int ans = Integer.MAX_VALUE;
+                for(int k=i+1;k<j;k++)
+                {
+                    ans = Math.min(ans, values[i]*values[k]*values[j] + dp[i][k] + dp[k][j]);
+                }
+                dp[i][j] = ans;
+            }
         }
-        dp[i][j] = ans;
-        return dp[i][j];
+        return dp[0][n-1];
     }
     public int minScoreTriangulation(int[] values) {
         // return solve(values, 0, values.length-1);
         
-        int[][] dp = new int[values.length][values.length];
-        for(int i=0;i<values.length;i++)
-            Arrays.fill(dp[i], -1);
-        return solvedp(values, 0, values.length-1, dp);
+        // int[][] dp = new int[values.length][values.length];
+        // for(int i=0;i<values.length;i++)
+        //     Arrays.fill(dp[i], -1);
+        // return solvedp(values, 0, values.length-1, dp);
+        
+        return solvetab(values);
     }
 }
