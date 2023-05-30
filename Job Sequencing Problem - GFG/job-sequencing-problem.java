86dd2f5bda361c8316d1726e4c6edc3f8cc6fbe1
Himanshu <95352548+himanshu-rai1121@users.noreply.github.com>
2023-05-30 23:09:02 +0530

    Added solution - LeetHub

---
+++ Job Sequencing Problem - GFG/job-sequencing-problem.java
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+
+class GfG {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        //testcases
+		int t = Integer.parseInt(br.readLine().trim());
+		while(t-->0){
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //size of array
+            int n = Integer.parseInt(inputLine[0]);
+            Job[] arr = new Job[n];
+            inputLine = br.readLine().trim().split(" ");
+            
+            //adding id, deadline, profit
+            for(int i=0, k=0; i<n; i++){
+                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+            }
+            
+            Solution ob = new Solution();
+            
+            //function call
+            int[] res = ob.JobScheduling(arr, n);
+            System.out.println (res[0] + " " + res[1]);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n)
+    {
+        // Your code here
+        Arrays.sort(arr, (a, b)->(Integer.compare(b.profit, a.profit) ));
+        
+        int maxideadline = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            maxideadline = Math.max(maxideadline, arr[i].deadline);
+        }
+        int[] schedule = new int[maxideadline+1];
+        Arrays.fill(schedule, -1);
+        
+        int count = 0;
+        int maxProfit = 0;
+        for(int i=0;i<n;i++)
+        {
+            int currprof = arr[i].profit;
+            int currjobid = arr[i].id;
+            int currdead = arr[i].deadline;
+            
+            for(int k=currdead;k>0;k--)
+            {
+                if(schedule[k]==-1)
+                {
+                    count++;
+                    maxProfit+=currprof;
+                    schedule[k] = currjobid;
+                    break;
+                }
+            }
+        }
+        int[] result = new int[2];
+        result[0] = count;
+        result[1] = maxProfit;
+        
+        return result;
+        
+    }
+}
+
+/*
+class Job {
+    int id, profit, deadline;
+    Job(int x, int y, int z){
+        this.id = x;
+        this.deadline = y;
+        this.profit = z; 
+    }
+}
+*/
